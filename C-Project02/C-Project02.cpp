fc587adf1a71837636190f439a03e066fe8de2b8
KatanMichael <32782995+KatanMichael@users.noreply.github.com>
2017-12-02 21:22:59 +0200

    Update C-Project02.cpp

--- C-Project02/C-Project02.cpp
+++ C-Project02/C-Project02.cpp
@@ -1,4 +1,4 @@
-﻿// C-Project02.cpp : Defines the entry point for the console application.
+// C-Project02.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -9,9 +9,13 @@ using namespace std;
 
 void main()
 {
+	int stop;
 	Matrix m(4, 4);
 	cout << "Man You Are Ugly" <<endl;
 	
+	cout << m << endl;
+	
+	cin >> stop;
 
 }
 
